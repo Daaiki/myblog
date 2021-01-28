c2fbd15f1dc0a2fd3202d2530910b90592a5d854
Daaiki <daaikii@outlook.jp>
2021-01-28 09:26:25 +0900

    init: :tada: first commit!

---
+++ layouts/error.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    <h1 class="text-grey-600 text-2xl">
+      お探しのページは見つかりませんでした。
+    </h1>
+  </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="container mx-auto">
+    <h1 class="text-center text-green-700 text-6xl font-semibold">
+      Hello Nuxt
+    </h1>
+  </div>
+</template>
