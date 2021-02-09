b514a3f2a67b661a95b4030b9e82a754af1f138b
Daaiki <daaikii@outlook.jp>
2021-02-09 21:50:02 +0900

    feat: :sparkles: display a list of Card component

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,19 @@
 <template>
-  <Header />
+  <div>
+    <Header />
+    <Main>
+      <Card v-for="(article, index) in articles" :key="index" :title="article.title" :description="article.description" />
+    </Main>
+  </div>
 </template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  async asyncData ({ $content }) {
+    const articles = await $content('blog').fetch()
+    return { articles }
+  }
+})
+</script>
