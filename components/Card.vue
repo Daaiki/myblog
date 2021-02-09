28e89cf7fbfcf3474a217792c0c180b7ac1705cf
Daaiki <daaikii@outlook.jp>
2021-02-09 21:43:46 +0900

    feat: :sparkles: add Card component

---
+++ components/Card.vue
@@ -0,0 +1,20 @@
+<template>
+  <article class="pl-10 mb-10">
+    <h2 class="text-2xl text-green-400 font-bold">{{ title }}</h2>
+    <p class="text-gray-650 py-4 pl-3">{{ description }}</p>
+  </article>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  props: {
+    title: {
+      type: String
+    },
+    description: {
+      type: String
+    },
+  }
+})
+</script>
