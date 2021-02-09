26bc2405ef218ac3ab6318e9ad763f15a636ada5
Daaiki <daaikii@outlook.jp>
2021-02-09 21:13:48 +0900

    ui: :lipstick: add default background color

--- layouts/default.vue
+++ layouts/default.vue
@@ -8,6 +8,7 @@
 html {
   font-family: 'Noto Sans JP', sans-serif;
   font-size: 16px;
+  background-color: #fafafa;
   -moz-osx-font-smoothing: grayscale;
   -webkit-font-smoothing: antialiased;
 }
