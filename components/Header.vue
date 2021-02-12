589ecfdb4b0543933dcb5b9a9ed2776ace8e7d69
Daaiki <daaikii@outlook.jp>
2021-02-12 18:08:45 +0900

    feat: :sparkles: set tasty-burger-button in the header

--- components/Header.vue
+++ components/Header.vue
@@ -5,5 +5,17 @@
     >
       &#64;daiki&rsquo;s Blog
     </h1>
+    <div class="flex">
+      <SnsIcons class="pt-1" />
+      <div class="bg-pale-green h-24 w-24 flex justify-center items-center">
+        <tasty-burger-button
+          type="collapse"
+          active="true"
+          size="m"
+          color="#969383"
+          active-color="#5e5e5e"
+        />
+      </div>
+    </div>
   </header>
 </template>
