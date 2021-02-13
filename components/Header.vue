505ef7993bfef6e8541a87ffa1ad0d6af796e129
Daaiki <daaikii@outlook.jp>
2021-02-13 18:23:41 +0900

    feat: :sparkles: link title to index

--- components/Header.vue
+++ components/Header.vue
@@ -1,10 +1,12 @@
 <template>
   <header class="flex justify-between items-center z-50">
-    <h1
-      class="font-bold text-pale-gray tracking-wide text-xl sm:text-2xl md:text-3xl lg:pl-16 md:pl-10 sm:pl-6 pl-3 sm:my-6"
-    >
-      &#64;daiki&rsquo;s Blog
-    </h1>
+    <nuxt-link to="/">
+      <h1
+        class="font-bold text-pale-gray tracking-wide text-xl sm:text-2xl md:text-3xl lg:pl-16 md:pl-10 sm:pl-6 pl-3 sm:my-6"
+      >
+        &#64;daiki&rsquo;s Blog
+      </h1>
+    </nuxt-link>
     <div class="flex fixed right-0">
       <SnsIcons
         class="pt-1 bg-pale-white"
