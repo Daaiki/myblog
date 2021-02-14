8dc264e60310c69259e02b0a1a49bb0afc0d9915
Daaiki <daaikii@outlook.jp>
2021-02-14 16:23:56 +0900

    feat: :sparkles: add categories functions

---
+++ pages/categories/_category.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <Title>{{ $route.params.category }}</Title>
+    <Card
+      v-for="(article, index) in articles"
+      :key="index"
+      :title="article.title"
+      :date="article.date"
+      :category="article.category"
+      :tags="article.tags"
+      :slug="'/posts/' + article.slug"
+    />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  async asyncData({ $content, params }) {
+    const articles = await $content('posts', { deep: true })
+      .sortBy('id', 'desc')
+      .where({ category: { $contains: params.category } })
+      .fetch()
+    return {
+      articles,
+    }
+  },
+})
+</script>
---
+++ pages/categories/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <Title class="mb-20">Categories</Title>
+    <Categories
+      v-for="(category, index) in categories"
+      :key="index"
+      :category="category.category"
+    />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  async asyncData({ $content }) {
+    const query = $content('posts', { deep: true }).only(['category'])
+    const categories = await query.fetch()
+    return {
+      categories,
+    }
+  },
+})
+</script>
