0d9b99f116222827b3acdf121988d7a2fcdd68e3
Daaiki <daaikii@outlook.jp>
2021-02-14 20:44:11 +0900

    feat: :sparkles: add link

--- components/SideBar.vue
+++ components/SideBar.vue
@@ -1,12 +1,11 @@
 <template>
   <nav
-    class="h-screen fixed top-0 right-0 bg-pale-green text-pale-gray z-0 w-48"
+    class="h-screen fixed top-0 right-0 bg-pale-green text-pale-gray z-0 xs:w-48 w-full"
   >
     <ul class="pt-48 pb-24 pl-12">
-      <li class="mb-10">Posts</li>
-      <li class="mb-10">About</li>
-      <li class="mb-10">Categories</li>
-      <li>Tags</li>
+      <li class="mb-10"><nuxt-link to="/">Posts</nuxt-link></li>
+      <li class="mb-10"><nuxt-link to="categories">Categories</nuxt-link></li>
+      <li class="mb-10"><nuxt-link to="about">About</nuxt-link></li>
     </ul>
     <SnsIcons width="20" height="20" classes="cursor-pointer pr-3" />
   </nav>
