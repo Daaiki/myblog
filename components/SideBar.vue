1be44b58c49aa4bf96e090875e2d4820cd582293
Daaiki <daaikii@outlook.jp>
2021-02-15 20:09:24 +0900

    ux: :children_crossing: change color on mouse hover

--- components/SideBar.vue
+++ components/SideBar.vue
@@ -3,9 +3,15 @@
     class="h-screen fixed top-0 right-0 bg-pale-green text-pale-gray z-0 xs:w-48 w-full"
   >
     <ul class="pt-48 pb-24 pl-12">
-      <li class="mb-10"><nuxt-link to="/">Posts</nuxt-link></li>
-      <li class="mb-10"><nuxt-link to="categories">Categories</nuxt-link></li>
-      <li class="mb-10"><nuxt-link to="about">About</nuxt-link></li>
+      <li class="mb-10 hover:text-hover-gray">
+        <nuxt-link to="/">Posts</nuxt-link>
+      </li>
+      <li class="mb-10 hover:text-hover-gray">
+        <nuxt-link to="categories">Categories</nuxt-link>
+      </li>
+      <li class="mb-10 hover:text-hover-gray">
+        <nuxt-link to="about">About</nuxt-link>
+      </li>
     </ul>
     <SnsIcons width="20" height="20" classes="cursor-pointer pr-3" />
   </nav>
