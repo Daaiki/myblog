42bca40bb47b8526022b2d36aaefa76588292a36
Daaiki <daaikii@outlook.jp>
2021-02-23 21:57:15 +0900

    refactor: :recycle: move hamburger icon from header to sidebar

--- components/SideBar.vue
+++ components/SideBar.vue
@@ -1,18 +1,47 @@
 <template>
-  <nav
-    class="h-screen fixed top-0 right-0 bg-pale-green text-pale-gray z-0 w-48"
-  >
-    <ul class="pt-48 pb-24 pl-12">
-      <li class="mb-10 hover:text-hover-gray">
-        <nuxt-link to="/">Posts</nuxt-link>
-      </li>
-      <li class="mb-10 hover:text-hover-gray">
-        <nuxt-link to="../categories">Categories</nuxt-link>
-      </li>
-      <li class="mb-10 hover:text-hover-gray">
-        <nuxt-link to="../about">About</nuxt-link>
-      </li>
-    </ul>
-    <SnsIcons width="20" height="20" classes="cursor-pointer pr-3" />
-  </nav>
+  <div>
+    <div>
+      <unicon
+        :name="isHidden ? 'bars' : 'times'"
+        fill="#969383"
+        width="45"
+        height="45"
+        class="z-20 fixed right-0 top-0 mr-3 mt-3 md:mr-6 md:mt-6 cursor-pointer"
+        @click="onClick"
+      />
+    </div>
+    <nav
+      class="h-screen fixed top-0 right-0 bg-pale-green text-pale-gray z-0 w-48"
+      :class="{ hidden: isHidden }"
+    >
+      <ul class="pt-48 pb-24 pl-12">
+        <li class="mb-10 hover:text-hover-gray">
+          <nuxt-link to="/">Posts</nuxt-link>
+        </li>
+        <li class="mb-10 hover:text-hover-gray">
+          <nuxt-link to="../categories">Categories</nuxt-link>
+        </li>
+        <li class="mb-10 hover:text-hover-gray">
+          <nuxt-link to="../about">About</nuxt-link>
+        </li>
+      </ul>
+      <SnsIcons width="20" height="20" classes="cursor-pointer pr-3" />
+    </nav>
+  </div>
 </template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  data() {
+    return {
+      isHidden: true,
+    }
+  },
+  methods: {
+    onClick() {
+      this.isHidden = !this.isHidden
+    },
+  },
+})
+</script>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,34 +1,18 @@
 <template>
   <!-- sticky footer -->
   <div class="min-h-screen flex flex-col">
-    <Header @hamburgerClick="hiddenSidebar" />
+    <Header />
     <div class="flex flex-1">
       <Container>
         <Main>
           <Nuxt />
         </Main>
       </Container>
-      <SideBar :class="{ hidden: isHidden }" />
+      <SideBar />
     </div>
     <Footer />
   </div>
 </template>
-
-<script lang="ts">
-import Vue from 'vue'
-export default Vue.extend({
-  data() {
-    return {
-      isHidden: true,
-    }
-  },
-  methods: {
-    hiddenSidebar() {
-      this.isHidden = !this.isHidden
-    },
-  },
-})
-</script>
 
 <style>
 html {
